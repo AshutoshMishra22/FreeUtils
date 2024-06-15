db21587c7391e20cc51e76df1c9991a53abff1ff
AshutoshMishra22 <ashutoshmishraofficial22@gmail.com>
2024-06-15 22:08:07 +0530

    base setup in next 14

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Header from '@/component/Header';
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <Header />
+    </main>
+  );
+}
